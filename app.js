5ed3e5fdafaeae8946de1b2da2e8e8e847f07de5
Christopher White <x0tion007@gmail.com>
2022-09-01 02:43:07 +0100

    handlebar initaliazed

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express')
 const colors = require('colors')
 const exphbs = require('express-handlebars')
 const application = express()
+const { engine } =require('express-handlebars');
 const path = require('path')
-
-//  set middleware for express
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './public');
 
 
 // set static fiolder
 application.use(express.static(path.join(__dirname, 'public')))
+
+//  set middleware for express
+application.engine('handlebars', engine());
+application.set('view engine', 'handlebars');
+application.set('views', './views');
+
+application.get('/', (req, res) => {
+    res.render('home')
+})
+
 
 
 const PORT = process.env.PORT || 5000
